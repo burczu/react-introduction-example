7c995bbba86d9d5c250fa275398aa5bf2a46f811
burczu <b.dybowski@gmail.com>
2016-11-19 11:40:05 +0100

    internal state + binding added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Text from './Text';
 
 class AppComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = { text: 'Not clicked!' };
+  }
+
+  onClick() {
+    this.setState({ text: 'Clicked!' });
+  }
+
   render() {
     return (
       <div className="main">
-        <p>Hello World!</p>
+        <Text />
+        <p>{this.state.text}</p>
+        <button onClick={this.onClick.bind(this)}>Click</button>
       </div>
     );
   }
